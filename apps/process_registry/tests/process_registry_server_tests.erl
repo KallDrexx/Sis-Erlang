d9ceae80c72e57fa5f9fbd249b90dcc7109806c7
KallDrexx <me@mshapiro.net>
2016-03-27 21:48:49 -0400

    Added pid overwrite verification.
    Test proves you can overwrite an existing registered pidkey with a new pid.

--- apps/process_registry/tests/process_registry_server_tests.erl
+++ apps/process_registry/tests/process_registry_server_tests.erl
@@ -5,6 +5,9 @@
 can_register_and_retrieve_pid_via_key_test_() ->
   {"Registers self() and retrieves self() back with same key", ?setup(fun can_register_and_get_pid/1)}.
 
+can_overwrite_existing_pid_test_() ->
+  {"Registers two pids with the same key, and retrieval gets the 2nd pid", ?setup(fun can_overwrite_existing_pid/1)}.
+
 %% Setup functions
 start() ->
   {ok, ServerPid} = process_registry_server:start_link(),
@@ -16,5 +19,12 @@ stop(ServerPid) -> gen_server:stop(ServerPid).
 can_register_and_get_pid(ServerPid) ->
   ExpectedPid = self(),
   ok = process_registry_server:register_pid(ServerPid, ExpectedPid, "abc"),
-  {ok, ReturnedPid} = process_registry_server:get_pid(ServerPid, "abc"),
-  ?_assertEqual(ExpectedPid, ReturnedPid).
+  ?_assertMatch({ok, ExpectedPid}, process_registry_server:get_pid(ServerPid, "abc")).
+
+can_overwrite_existing_pid(ServerPid) ->
+  SelfPid = self(),
+  SecondPid = spawn(fun() -> ok end),
+
+  ok = process_registry_server:register_pid(ServerPid, SelfPid, "abc"),
+  ok = process_registry_server:register_pid(ServerPid, SecondPid, "abc"),
+  ?_assertMatch({ok, SecondPid}, process_registry_server:get_pid(ServerPid, "abc")).
