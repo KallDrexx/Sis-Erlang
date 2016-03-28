cc6a75ede41d2518fa15afda41ad1465e4a4198b
KallDrexx <me@mshapiro.net>
2016-03-27 21:59:52 -0400

    added unregister verification test

--- apps/process_registry/src/process_registry_server.erl
+++ apps/process_registry/src/process_registry_server.erl
@@ -3,7 +3,7 @@
 -record(state, {known_pids}).
 
 %% API
--export([start_link/0, register_pid/3, unregister_pid/3, get_pid/2]).
+-export([start_link/0, register_pid/3, unregister_pid_key/2, get_pid/2]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 start_link() -> gen_server:start_link(?MODULE, [], []).
@@ -11,8 +11,8 @@ start_link() -> gen_server:start_link(?MODULE, [], []).
 register_pid(RegistryServerRef, PidToRegister, PidKey) ->
   gen_server:cast(RegistryServerRef, {register, {PidToRegister, PidKey}}).
 
-unregister_pid(RegistryServerRef, PidToUnregister, PidKey) ->
-  gen_server:cast(RegistryServerRef, {unregister, {PidToUnregister, PidKey}}).
+unregister_pid_key(RegistryServerRef, PidKey) ->
+  gen_server:cast(RegistryServerRef, {unregister, PidKey}).
 
 get_pid(RegistryServerRef, PidKey) ->
   gen_server:call(RegistryServerRef, {get_pid, PidKey}).
@@ -28,12 +28,14 @@ handle_call({get_pid, PidKey}, _From, State=#state{}) ->
 
 handle_cast({register, {PidToRegister, PidKey}}, State=#state{known_pids = KnownPids}) ->
   NewState = State#state{known_pids = orddict:store(PidKey, PidToRegister, KnownPids)},
+  {noreply, NewState};
+
+handle_cast({unregister, PidKey}, State=#state{known_pids = KnownPids}) ->
+  NewState = State#state{known_pids = orddict:erase(PidKey, KnownPids)},
   {noreply, NewState}.
 
 handle_info(_Info, _State) ->
   erlang:error(not_implemented).
 
 terminate(_Reason, _State) -> ok.
-
-code_change(_OldVsn, _State, _Extra) ->
-  erlang:error(not_implemented).
+code_change(_OldVsn, State, _Extra) ->  {ok, State}.
--- apps/process_registry/tests/process_registry_server_tests.erl
+++ apps/process_registry/tests/process_registry_server_tests.erl
@@ -10,8 +10,12 @@ can_overwrite_existing_pid_test_() ->
   {"Registers two pids with the same key, and retrieval gets the 2nd pid",
     ?setup(fun can_overwrite_existing_pid/1)}.
 
-unregistered_pid_test_() ->
+non_registered_pid_key_test_() ->
   {"Verifies retrieving a pid key that hasn't been registered gives undefined",
+    ?setup(fun pidkey_not_registered_returns_undefined/1)}.
+
+unregistered_pid_key_test_() ->
+  {"Verfies pid keys can be unregistered",
     ?setup(fun unregistered_pidkey_returns_undefined/1)}.
 
 %% Setup functions
@@ -35,5 +39,11 @@ can_overwrite_existing_pid(ServerPid) ->
   ok = process_registry_server:register_pid(ServerPid, SecondPid, "abc"),
   ?_assertMatch({ok, SecondPid}, process_registry_server:get_pid(ServerPid, "abc")).
 
+pidkey_not_registered_returns_undefined(ServerPid) ->
+  ?_assertMatch(undefined, process_registry_server:get_pid(ServerPid, "abc")).
+
 unregistered_pidkey_returns_undefined(ServerPid) ->
+  SelfPid = self(),
+  ok = process_registry_server:register_pid(ServerPid, SelfPid, "abc"),
+  ok = process_registry_server:unregister_pid_key(ServerPid, "abc"),
   ?_assertMatch(undefined, process_registry_server:get_pid(ServerPid, "abc")).
